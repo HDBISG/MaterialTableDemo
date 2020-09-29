c5ad806a74e31728831949872ab29da542a3f8e8
ZhangJi <zhangji@hotmail.com>
2020-09-29 17:26:29 +0800

    ..

--- src/component/URITable.js
+++ src/component/URITable.js
@@ -1,50 +1,12 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import TTGWApi, {RequestConfig} from "../api/remoteServer";
 import MaterialTable from 'material-table';
-
-import { forwardRef } from 'react';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
+import TTGWApi, {RequestConfig} from "../api/remoteServer";
+import CommonTable from "./common/CommonTable";
 import Refresh from '@material-ui/icons/Refresh';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
 
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-  Refresh: forwardRef((props, ref) => <Refresh {...props} ref={ref} />)
-};
 
 const useStyles = (theme) => ({
   root: {
@@ -57,7 +19,18 @@ const useStyles = (theme) => ({
 
 const tableRef = React.createRef();
 
-class ListTransactions extends React.Component {
+
+const columns = [
+  { title: 'id', field: 'modelID' },
+  { title: 'appsCode', field: 'appsCode' },
+  { title: 'status', field: 'uriStatus' 
+    , lookup: { "A":"Active", "I":"Inactive"},},
+  { title: 'type', field: 'uriType' },
+  { title: 'path', field: 'uriDetails' },
+  { title: 'active date', field: 'uriDtActive' }
+];
+
+class ListTransactions extends CommonTable {
   constructor(props) {
     super(props);
   }
@@ -72,6 +45,8 @@ class ListTransactions extends React.Component {
   render() {
     const { classes } = this.props;
 
+    console.log("super.tableIcons = " + super.getTableIcons() );
+
     return (
       <React.Fragment>
         <Typography variant="h6" gutterBottom>
@@ -79,34 +54,22 @@ class ListTransactions extends React.Component {
         </Typography>
         <Grid container spacing={3}>
 
-
           <Grid item xs={12} sm={12}>
             <Grid container spacing={3}>
               <Grid item xs={12} sm={12}>
               <MaterialTable
                 tableRef={tableRef}
-                icons={tableIcons}
+                icons={ super.getTableIcons() }
                 title=""
-                columns={[
-                  { title: 'id', field: 'modelID' },
-                  { title: 'appsCode', field: 'appsCode' },
-                  { title: 'status', field: 'uriStatus' 
-                    , lookup: { "A":"Active", "I":"Inactive"},},
-                  { title: 'type', field: 'uriType' },
-                  { title: 'path', field: 'uriDetails' },
-                  { title: 'active date', field: 'uriDtActive' }
-                ]}
+                columns= {columns}
                 onSearchChange = {() => {
-                  //console.log(tableRef.current.DataManager.filteredData.length);
-                  console.log("onSearchChange");
+                  console.log(`onSearchChange`);
                 }}
                 onOrderChange = {() => {
-                  //console.log(tableRef.current.DataManager.filteredData.length);
-                  console.log("onOrderChange");
+                  console.log(`onOrderChange`);
                 }}
                 onSelectionChange = {() => {
-                  //console.log(tableRef.current.DataManager.filteredData.length);
-                  console.log("onSelectionChange");
+                  console.log(`onSelectionChange`);
                 }}
                // data = {this.state.details}
               data={query =>
